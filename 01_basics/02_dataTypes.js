732f6434c91dd370c975ce0afd1b16eb7d77bb02
Raushan-9523 <66361203+Raushan-9523@users.noreply.github.com>
2024-01-06 09:58:56 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+
+//number
+//string
+//bigint
+//symbol
+//boolean => true/false
+//null
+//undefined
+
+//typeof
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
