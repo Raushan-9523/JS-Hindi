17248294626d2436aeeee5f7b79e0d52d1c91753
Raushan-9523 <66361203+Raushan-9523@users.noreply.github.com>
2024-01-07 01:03:07 +0000

    Math and num

---
+++ 01_basics/05_nums_and_math.js
@@ -0,0 +1,26 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100.26589);
+
+// console.log(balance);
+
+// console.log(balance.toFixed(1));
+
+const anotherNumber = 123.7458;
+// console.log(anotherNumber.toPrecision(3));
+
+const hundreds = 10000000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/************************Math**************/
+
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(78.956));
+// console.log(Math.min(1,2,3,4,5,6));
+
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
