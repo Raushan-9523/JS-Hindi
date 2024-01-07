84befe90b371c8389e0f48e51cf70a96a0e9cab1
Raushan-9523 <66361203+Raushan-9523@users.noreply.github.com>
2024-01-07 01:25:06 +0000

    dates and time

---
+++ 01_basics/06_datesInJS.js
@@ -0,0 +1,18 @@
+let myDate  = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+ let myCreatedDate = new Date();
+// console.log(myCreatedDate);
+
+let myTimeStap = Date.now();
+
+// console.log(myTimeStap);
+
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+console.log(myCreatedDate.getDay());
