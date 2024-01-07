c4f05de3896e49f22f80808ee5b1c43d13f6e7c9
Raushan-9523 <66361203+Raushan-9523@users.noreply.github.com>
2024-01-07 00:19:22 +0000

    strings

---
+++ 01_basics/04_string.js
@@ -0,0 +1,23 @@
+const name = "Raushan";
+const lastName = "Tiwari";
+
+console.log(`Hello My Name ${name}`);
+
+const gameName = new String("Tiwari");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('i'));
+// console.log(gameName.substring(0,4));
+// console.log(gameName.slice(-1,3));
+
+const newString = "rtr.ttl";
+// console.log((newString.trim()));
+
+// console.log(newString.includes("ttl"));
+// console.log(newString.replace("ttl","tcs"));
+// console.log(newString.split("."));
+
